be185d94296ae1a5917091f34957ad693fc3b724
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-08 23:03:45 +0300

    Add transaction signs

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className="minus">
-      {transaction.text} <span>${transaction.amount}</span>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
